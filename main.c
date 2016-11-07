c1c2f63baced2a839a6385352f48c6e61f7b94b2
remyCESI <remy.vancamp@viacesi.fr>
2016-11-07 13:48:15 +0100

    renseigner un message de commit

--- main.c
+++ main.c
@@ -9,6 +9,6 @@ int addition(int a, int b)
 
 int main(){
     printf("Hello world!\n");
-    printf("%d\n ", addition(1,4));
+    printf("L'addition de a et b vaut %d\n ", addition(1,4));
     return 0;
 }
